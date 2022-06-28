9f8f2b6842bc2978a4fb021fa9586c39e203254d
Josh <88389161+joshh01@users.noreply.github.com>
2022-06-28 19:38:50 -0400

    Update Main.java

--- Login System/Main.java
+++ Login System/Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+
+import javax.swing.JLabel;
 public class Main 
 {
 	public static void main(String[] args) throws IOException
@@ -24,10 +26,25 @@ public class Main
 			idandPasswords.addInfo(usernames.get(i), passwords.get(i));
 		}
 		// Creates login page
-		new LoginPage(idandPasswords.getLoginInfo());
+		LoginPage lp = new LoginPage(idandPasswords.getLoginInfo());
 		scanner.close();
+		
+		
+		
+		for(int i = 0; i < usernames.size(); i++)
+		{
+			JLabel label = new JLabel(usernames.get(i));
+			JLabel pass = new JLabel(passwords.get(i));
+			label.setBounds(5, ((i - 1) * 20) + 5, 100, 50);
+			pass.setBounds(55, ((i - 1) * 20) + 5, 100, 50);
+			lp.addLabel(label);
+			lp.addLabel(pass);
+		}
+		
+		
+		
 	}
-	// Splits xxx:yyy into xxx & yyy
+	// Splits user:pass into user & pass
 	public static void split(ArrayList<String> arr, ArrayList<String> usernames, ArrayList<String> passwords)
 	{
 		int colonIndex = -1;
@@ -42,10 +59,14 @@ public class Main
 					colonIndex = j;
 				}
 			}
-			username = data.substring(0, colonIndex);
-			password = data.substring(colonIndex + 1, data.length());
-			usernames.add(username);
-			passwords.add(password);
+			// Makes sure that the string contains a ":", that there is data before and after the colon
+			if(arr.get(i).contains(":") && colonIndex != 0 && colonIndex != arr.get(i).length() - 1)
+			{
+				username = data.substring(0, colonIndex);
+				password = data.substring(colonIndex + 1, data.length());
+				usernames.add(username);
+				passwords.add(password);
+			}
 		}
 	}
 }
